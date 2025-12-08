e479e200415827f4ee2feac7262561e5d4e0df38
betterb2etr <b2etr@naver.com>
2025-12-08 16:06:12 +0900

    config: project init

---
+++ concert_appetizer/src/main/java/com/ohgiraffers/concert_appetizer/ConcertAppetizerApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.concert_appetizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertAppetizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertAppetizerApplication.class, args);
+    }
+
+}
---
+++ concert_appetizer/src/test/java/com/ohgiraffers/concert_appetizer/ConcertAppetizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.concert_appetizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertAppetizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
